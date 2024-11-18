a89b160027774df087a32357f6556111ff29436d
THK2004 <84148247+THK2004@users.noreply.github.com>
2024-11-18 20:38:12 +0700

    Add Waiting for data to be fetch

--- shop/controllers/glassesController.js
+++ shop/controllers/glassesController.js
@@ -1,15 +1,25 @@
-var products = require('../models/product');
+const productsModel = require('../models/product');
 
-const getGlasses = (req, res) => {
-    res.render('glasses/glasses', {glasses: true, products});
-}
+const getGlasses = async (req, res) => {
+  try {
+    const products = await productsModel.run();
+    res.render('glasses/glasses', { glasses: true, products });
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+};
 
-const getProductDetail = (req, res) => {
+const getProductDetail = async (req, res) => {
+  try {
+    const products = await productsModel.run();
     const product = products.find(p => p.id === req.params.id);
-    res.render('products/product', {glasses: true, product});
-}
+    res.render('products/product', { glasses: true, product });
+  } catch (error) {
+    console.error('Error fetching product:', error);
+  }
+};
 
 module.exports = {
-    getGlasses,
-    getProductDetail
-}
+  getGlasses,
+  getProductDetail
+};
